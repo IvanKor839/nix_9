ce3dc2220259e1a9f2916dcef117832a5d6d30de
Iegor-Funtusov <funtushan@gmail.com>
2021-10-24 12:12:04 +0300

    add review.txt

--- hw_1_base_operations/src/main/java/ua/com/alevel/Main.java
+++ hw_1_base_operations/src/main/java/ua/com/alevel/Main.java
@@ -4,12 +4,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class main {
+public class Main {
+
     public static void main(String[] args) throws IOException {
         List<TaskHelper> taskHelpers = new ArrayList<>();
         taskHelpers.add(new SumNumbersAndSortLetters());
         taskHelpers.add(new EndLessons());
         ProgramRun.run(taskHelpers);
-
     }
 }
